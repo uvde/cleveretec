08177ff7fc504610642bf278336f23c938bbe34f
uvde <uvd.1994@gmail.com>
2022-12-29 21:00:45 +0300

    add annotation @RestController to CheckController class

--- src/main/java/ru/clevertec/vasili/urusov/controller/CheckController.java
+++ src/main/java/ru/clevertec/vasili/urusov/controller/CheckController.java
@@ -5,16 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.clevertec.vasili.urusov.model.dto.OrderOfClientDTO;
 import ru.clevertec.vasili.urusov.service.CheckService;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/check")
 public class CheckController {
